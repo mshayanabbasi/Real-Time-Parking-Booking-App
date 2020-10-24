6cc843fa21fea0c6a8d34897f49f5086d00a2077
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-10-24 16:29:56 +0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1}}>
+      <Text>App.js</Text>
+    </View>
+  );
+};
+
+export default App;

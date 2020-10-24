2f47c547efc0d4cc0cd5d04d6b32dca74f3cfe60
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-10-24 17:02:38 +0500

    auth type

---
+++ src/store/types/index.js
@@ -0,0 +1,12 @@
+// auth type
+export const SIGNUP_LOADING = 'SIGNUP_LOADING';
+export const SIGNUP_SUCCESS = 'SIGNUP_SUCCESS';
+export const SIGNUP_ERROR = 'SIGNUP_ERROR';
+
+export const SIGNIN_LOADING = 'SIGNIN_LOADING';
+export const SIGNIN_SUCCESS = 'SIGNIN_SUCCESS';
+export const SIGNIN_ERROR = 'SIGNIN_ERROR';
+
+export const SIGNOUT_LOADING = 'SIGNOUT_LOADING';
+export const SIGNOUT_SUCCESS = 'SIGNOUT_SUCCESS';
+export const SIGNOUT_ERROR = 'SIGNOUT_ERROR';

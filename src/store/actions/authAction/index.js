edc3e5ef5c44db9d9a2f0cdd2890e5a0d6db9eee
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-10-24 17:02:28 +0500

    auth actions

---
+++ src/store/actions/authAction/index.js
@@ -0,0 +1,47 @@
+import firestore from '@react-native-firebase/firestore';
+import auth from '@react-native-firebase/auth';
+import {
+  SIGNIN_ERROR,
+  SIGNIN_LOADING,
+  SIGNUP_ERROR,
+  SIGNUP_LOADING,
+  SIGNUP_SUCCESS,
+} from '../../types';
+
+export const signUp = (userData) => {
+  return async (dispatch) => {
+    try {
+      dispatch({type: SIGNUP_LOADING});
+      var response = await auth().createUserWithEmailAndPassword(
+        userData.email,
+        userData.password,
+      );
+      let obj = {
+        name: userData.name,
+        email: userData.email,
+        uid: response.user.uid,
+        userType: 'user',
+      };
+      firestore().collection('users').doc(response.user.uid).set({
+        obj,
+      });
+      dispatch({type: SIGNUP_SUCCESS, payload: obj});
+    } catch (error) {
+      dispatch({type: SIGNUP_ERROR, payload: error});
+    }
+  };
+};
+
+export const signIn = (userData) => {
+  return async (dispatch) => {
+    try {
+      dispatch({type: SIGNIN_LOADING});
+      var response = await auth().signInWithEmailAndPassword(
+        userData.email,
+        userData.password,
+      );
+    } catch (error) {
+      dispatch({type: SIGNIN_ERROR, payload: error});
+    }
+  };
+};

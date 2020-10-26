48505eb60301a1ec772ffdbf244eb84537f73648
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-10-26 23:06:18 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 import SignInScreen from './screens/SignInScreen';
-import SignUpScreen from './screens/SignUpScreen';
+// import SignUpScreen from './screens/SignUpScreen';
+import {Provider} from 'react-redux';
+import store from './store';
 
 const App = () => {
   return (
-    <View style={{flex: 1}}>
-      <SignUpScreen />
-    </View>
+    <Provider store={store}>
+      <SignInScreen />
+    </Provider>
   );
 };
 

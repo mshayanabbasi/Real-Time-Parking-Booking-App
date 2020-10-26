7bfd049386805767789f16383eb0223c36ec3503
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-10-26 19:57:49 +0500

    Changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import SignInScreen from './screens/SignInScreen';
+import SignUpScreen from './screens/SignUpScreen';
 
 const App = () => {
   return (
     <View style={{flex: 1}}>
-      <Text>App.js</Text>
+      <SignUpScreen />
     </View>
   );
 };

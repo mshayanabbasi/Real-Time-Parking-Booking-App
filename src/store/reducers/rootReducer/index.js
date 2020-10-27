4076ff58144677925477e8fe8468009bb6930692
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-10-27 21:56:43 +0500

    add user Reducer in root Reducer

--- src/store/reducers/rootReducer/index.js
+++ src/store/reducers/rootReducer/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import authReducer from '../authReducer';
+import userReducer from '../rootReducer';
 
 const rootReducer = combineReducers({
-  auth: authReducer,
+  authReducer,
+  userReducer,
 });
 
 export default rootReducer;
